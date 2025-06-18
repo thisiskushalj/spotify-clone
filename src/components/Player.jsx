e23592221dbb4f184ea6db0fdbc4f4f055f283a7
Kushal J <kushalj400231@gmail.com>
2025-06-18 09:51:37 +0530

    Make player bar fixed at bottom on mobile

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,7 +6,7 @@ const Player = () => {
 
     const { track, seekBar, seekBg, playStatus, play, pause, time, previous, next, seekSong } = useContext(PlayerContext);
   return (
-    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
+    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4 lg:static fixed bottom-0 left-0 w-full z-50'>
         <div className='hidden lg:flex items-center gap-4'>
             <img className='w-12' src={track.image} alt="" />
             <div>
